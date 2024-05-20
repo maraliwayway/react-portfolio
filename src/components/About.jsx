0d07c9100c5fc1323c1e1d2859b19d885096bbd0
Mara Liwayway <115133366+maraliwayway@users.noreply.github.com>
2024-05-20 00:06:10 -0700

    Added animation to Skills.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,17 @@ import { motion } from "framer-motion"
 const About = () => {
   return (
     <div className="pb-20 pt-20">
-        <h2 className="my-10 text-center text-4xl font-semibold">
+        <motion.h2 
+            whileInView={{ opacity: 1, y: 0}}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1}}
+            className="my-10 text-center text-4xl font-semibold">
             About Me
-        </h2>
+        </motion.h2>
         <motion.div 
             whileInView={{ opacity: 1, x:0 }}
             initial={{ opacity: 0, x: -100 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 1.5 }}
             className="flex items-center justify-center">
             <p> {ABOUT_TEXT}</p>
         </motion.div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,38 +4,84 @@ import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { RiReactjsLine } from "react-icons/ri"
 import { RiTailwindCssFill } from "react-icons/ri";
+import { motion } from "framer-motion"
+
+const iconVariants = (duration) => ({
+    initial: { y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+        }
+    },
+})
 
 const Skills = () => {
   return (
     <div className="pb-20">
-        <h2 className="my-20 text-center text-4xl font-semibold">
+        <motion.h2
+            whileInView={{ opacity: 1, y: 0}}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.5}}
+            className="my-20 text-center text-4xl font-semibold">
             Skills
-        </h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="p-4">
+        </motion.h2>
+        <motion.div 
+            whileInView={{ opacity: 1, x: 0}}
+            initial={{ opacity: 0, x: -100}}
+            transition={{ duration: 1.5}}
+            className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className="p-4">
                 <FaPython className="text-7xl text-cyan-500" />
-            </div>
+            </motion.div>
 
-            <div className="p-4">
+            <motion.div
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className="p-4">
                 <DiJavascript1 className="text-7xl text-cyan-500" />
-            </div>
+            </motion.div>
 
-            <div className="p-4">
+            <motion.div
+                variants={iconVariants(3.5)}
+                initial="initial"
+                animate="animate"
+                className="p-4">
                 <FaHtml5 className="text-7xl text-cyan-500" />
-            </div>
+            </motion.div>
 
-            <div className="p-4">
+            <motion.div
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate"
+                className="p-4">
                 <FaCss3Alt className="text-7xl text-cyan-500" />
-            </div>
+            </motion.div>
 
-            <div className="p-4">
+            <motion.div
+                variants={iconVariants(4.5)}
+                initial="initial"
+                animate="animate"
+                className="p-4">
                 <RiReactjsLine className="text-7xl text-cyan-500" />
-            </div>
+            </motion.div>
 
-            <div className="p-4">
+            <motion.div
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className="p-4">
                 <RiTailwindCssFill className="text-7xl text-cyan-500" />
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
