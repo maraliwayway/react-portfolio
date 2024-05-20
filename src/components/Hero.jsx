63ccbab61f49c4d21e0b88c671412217f5c73f79
Mara Liwayway <115133366+maraliwayway@users.noreply.github.com>
2024-05-19 23:37:07 -0700

    Added Hero.jsx animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,14 @@
 import profilePic from "../assets/maraProfile.png"
+import { motion } from "framer-motion"
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: { duration: 0.5, delay: delay },
+    },
+})
 
 const Hero = () => {
   return (
@@ -6,23 +16,37 @@ const Hero = () => {
         <div className="flex flex-wrap items-center ">
             <div className="w-full lg:w-3/5">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className=" text-6xl lg:mt-16 lg:text-8xl font-medium">
+                    <motion.h1 
+                        variants={container(0)}
+                        initial="hidden"
+                        animate="visible"
+                        className=" text-6xl lg:mt-16 lg:text-8xl font-medium">
                         Hi, I'm Mara 👋🏼
-                    </h1>
-                    <span className="font-light tracking-tight text-cyan-500 text-2xl lg:text-3xl pt-10">
+                    </motion.h1>
+
+                    <motion.span 
+                        variants={container(0.5)}
+                        initial="hidden"
+                        animate="visible"
+                        className="font-light tracking-tight text-cyan-500 text-2xl lg:text-3xl pt-10">
                         Aspiring Software Engineer in Surrey, BC
-                    </span>
+                    </motion.span>
                     
                 </div>
             </div>
             <div className="w-full flex items-center lg:w-2/5 lg:p-8">
                 <div className="flex items-center justify-center pt-10">
-                    <img className="rounded-full bg-cyan-500 shadow-lg shadow-cyan-500/50" src={profilePic} alt="Mara" />
+                    <motion.img 
+                        initial={{ x:100, opacity: 0}}
+                        animate={{ x: 0, opacity: 1}}
+                        transition={{ duration: 1, delay: 1.2 }}
+                        className="rounded-full bg-cyan-500 shadow-lg shadow-cyan-500/50" src={profilePic} alt="Mara" />
                 </div>
             </div>
         </div>
     </div>
   )
 }
+
 
 export default Hero
